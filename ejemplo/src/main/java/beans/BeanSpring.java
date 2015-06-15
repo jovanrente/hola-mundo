9c9b468c4bae4cd77bb14b3cb4fb47c3d84d1aec
jovanrente <jovanrente@gmail.com>
2015-06-15 09:46:54 -0500

    hoy
    test

---
+++ ejemplo/src/main/java/beans/BeanSpring.java
@@ -0,0 +1,15 @@
+package beans;
+
+public class BeanSpring {
+	private String mensage;
+
+	public String getMensage() {
+		return mensage;
+	}
+
+	public void setMensage(String mensage) {
+		this.mensage = mensage;
+	}
+	
+
+}
